a1fd11e4e82177b25a417c18191f75afda1b62f8
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-11-30 00:02:52 +0600

    for loop

---
+++ forLoop.py
@@ -0,0 +1,8 @@
+# country = str(input("Enter your country Name: "))
+
+# for letter in country:
+#     print(letter)
+
+
+for idx in range(5, 10):
+    print('Hello world')
---
+++ practice.py
@@ -0,0 +1,20 @@
+# price = -10
+# if price :
+#     print("Yes")
+# else :
+#     print("No")
+
+# for i in range(10): 
+#     if i==5:
+#         continue
+#     print(i,end=" ")
+
+
+# a="Bd"
+# for i in range(len(a)):
+#     print(a[i],end="")
+
+i=1
+while(i<3):
+    print(i)
+    i+=1
