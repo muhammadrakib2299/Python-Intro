4abeaf1218aa7400069c09e0ba4636bbf9c3c9a7
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-11-30 00:49:30 +0600

    basic function

---
+++ function.py
@@ -0,0 +1,15 @@
+# def doubleIt(num):
+#     return num *2
+
+# print(doubleIt(50))
+
+
+
+
+
+def doubleNumber(number):
+    print('Your Number: ', number * 2)
+
+    
+
+doubleNumber(number = int(input(("Enter a number i will give you double : "))))
