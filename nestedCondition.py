58d20cfb5b94d65f01a99a87ce3d4c1354e67aea
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-11-29 23:08:59 +0600

    nested condition

---
+++ nestedCondition.py
@@ -0,0 +1,20 @@
+# exam = 'BCS'
+# passed = False
+
+# if exam == 'BCS' and passed == True:
+#     print('Congrats mama. You passed the BCS exam')
+
+# else:
+#     print('You are fail. Try again')
+
+
+salary = 50000
+has_flat = True
+
+if salary > 35000:
+    if(has_flat == True):
+        print('You are eligble to try loan')
+    else: print('You are not eligble')
+
+else:
+    print('Try later')
