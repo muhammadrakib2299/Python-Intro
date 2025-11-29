638fb017cfbb65bdede0306cbc4a71df9b297af8
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-11-29 16:24:03 +0600

    operator

---
+++ oparator.py
@@ -0,0 +1,10 @@
+num1 = int(input('Enter 1st number : '))
+num2 = int(input('Enter 2nd number: '))
+
+if(num1 > num2):
+    print(num1, 'is bigger then ', num2)
+
+else:
+    print(num2, 'is bigger than ', num1)
+
+
