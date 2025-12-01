882b6493755deb91b85dff24d57fc45aab003e69
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-12-02 00:16:35 +0600

    pyautogui and opnCV

--- auto.py
+++ auto.py
@@ -1,4 +1,8 @@
 import pyautogui
-pyautogui.alert('This is an alert box.')
-pyautogui.confirm('Enter option.', buttons=['A', 'B', 'C'])
-    
+import time
+# pyautogui.alert('This is an alert box.')
+# pyautogui.confirm('Enter option.', buttons=['A', 'B', 'C'])
+print("Now i am writting something for you please wait")
+time.sleep(3)
+pyautogui.write('I love python', interval=0.25)
+pyautogui.press('enter')
