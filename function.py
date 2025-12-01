b85896c201c368cae4e51c5125c38913b960b974
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-12-01 22:53:03 +0600

    functon understand

--- function.py
+++ function.py
@@ -4,12 +4,30 @@
 # print(doubleIt(50))
 
 
+# def doubleNumber(number):
+#     print('Your Number: ', number * 2)
+# doubleNumber(number = int(input(("Enter a number i will give you double : "))))
+
+
+# def doubt_it(num):
+#     result = num * 2
+#     print(result)
+
+
+# # doubt_it(35)
+
+# def add(num1, num2):
+#     total = num1 + num2
+#     print(total)
+
+# add(10, 52)
 
 
 
-def doubleNumber(number):
-    print('Your Number: ', number * 2)
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
 
-    
 
-doubleNumber(number = int(input(("Enter a number i will give you double : "))))
+output = multiply(15,5)
+print(output)
