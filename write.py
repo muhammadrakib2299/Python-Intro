1f07c740f86d5ac69be7817bc9415f5818bd1705
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-12-02 00:33:45 +0600

    update

--- write.py
+++ write.py
@@ -1,7 +1,7 @@
 # with open ('massage.txt', 'w') as filewrite:
 #     filewrite.write('Hello from python, i am new learn')
 
-with open ('massage.txt', 'r') as fileRead:
-    text = fileRead.read()
+# with open ('massage.txt', 'r') as fileRead:
+#     text = fileRead.read()
 
-print(text)
+# print(text)
