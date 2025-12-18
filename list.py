8fb5e1b2082144ac548121afaa638cf2d978a5d4
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-12-19 00:54:34 +0600

    list topic

---
+++ list.py
@@ -0,0 +1,33 @@
+# numbers = [10, 20, 45, 65, 70, 11, 85, 12, 15, 17, 21, 26, 78]
+# print(numbers[1])
+# print(numbers[-5])
+
+# slice -> [start : end]
+# print(numbers[1: -1])
+
+# Slice 
+# print(numbers[0: 8: 2 ])
+
+# print(numbers[: :-1])
+
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+
+# Count 
+print('Count: ',fruits.count('apple'))
+# reverse 
+fruits.reverse()
+print('reverse : ', fruits)
+
+# Append - add item of the list 
+fruits.append('grape')
+print('After append: ', fruits)
+
+# sort - filtering items 
+fruits.sort()
+print('Sort: ', fruits)
+
+# Pop - remove an item of the end from the list
+fruits.pop()
+print('Pop : ', fruits)
+
