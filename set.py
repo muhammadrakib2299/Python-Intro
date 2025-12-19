251ac23a0afb3834e138b07382c58ab230461397
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-12-20 00:48:31 +0600

    set and tuple

---
+++ set.py
@@ -0,0 +1,9 @@
+# numbers = [2, 5, 10, 52, 14, 98]
+# print(numbers)
+
+# set - 
+nums = {10, 20, 30, 40, 50, 56, 37, 39, 10, 30 }
+print(nums)
+
+emty = set()
+print(emty)
---
+++ tuple.py
@@ -0,0 +1,7 @@
+# numbers = [10, 12, 14, 15, 16, 18, ]
+
+numbers_tupple = 12, 40, 36, 50, 80
+print(numbers_tupple)
+
+tuple2d = ([10, 20, 30], [15, 30, 45, 65, 80])
+print(tuple2d)
