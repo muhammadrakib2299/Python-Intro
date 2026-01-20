49716c8483e7941b5982f70efae659c743281254
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2026-01-21 00:18:07 +0600

    freecodecamp function

---
+++ FreeCodeCamp/Functions.py
@@ -0,0 +1,18 @@
+# Built in Functions
+
+# name = input("Enter you name: ")
+# print('Hello', name)
+
+# Convert to nuimeric value
+# print(int(3.105))
+
+
+def hello():
+    print('Hello')
+
+# hello()
+
+def calculate_sum(a, b):
+    print(a+b)
+
+# calculate_sum(10, 5)
