a3830f4e541f69eb88c064cdde21542e71ebfea0
Md. Rakib <muhammad.rakib2299@gmail.com>
2026-02-17 16:42:35 +0600

    leete code 19

---
+++ leetecode/19.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = dummy
+        
+        # Move fast pointer n steps ahead
+        for _ in range(n):
+            fast = fast.next
+        
+        # Move both pointers
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        
+        # Remove the nth node from end
+        slow.next = slow.next.next
+        
+        return dummy.next
