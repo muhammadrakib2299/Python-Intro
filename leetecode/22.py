f6b91f17c7b522cda36d4c5ff8f1cc452f539f15
Md. Rakib <muhammad.rakib2299@gmail.com>
2026-02-18 12:46:25 +0600

    leete code 22

---
+++ leetecode/22.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        
+        def backtrack(current, open_count, close_count):
+            # If the string is complete
+            if len(current) == 2 * n:
+                result.append(current)
+                return
+            
+            # Add '(' if we still can
+            if open_count < n:
+                backtrack(current + "(", open_count + 1, close_count)
+            
+            # Add ')' if it won't break validity
+            if close_count < open_count:
+                backtrack(current + ")", open_count, close_count + 1)
+        
+        backtrack("", 0, 0)
+        return result
