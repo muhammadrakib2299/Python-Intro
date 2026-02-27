1b62e0a307f4da2f7b5cdf842cd4d6e56a1af6bd
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2026-02-27 09:13:10 +0600

    upate leete code 3666

--- leetecode/3666.py
+++ leetecode/3666.py
@@ -6,18 +6,18 @@ class Solution:
         if zeros == 0:
             return 0
         
-        if k > n:
-            return -1
-        
         # If k is even
         if k % 2 == 0:
-            # x*k is always even
+            # Total parity cannot change
             if zeros % 2 == 1:
                 return -1
             return (zeros + k - 1) // k
         
         # If k is odd
         x = (zeros + k - 1) // k
+        
+        # parity condition
         if x % 2 != zeros % 2:
             x += 1
+        
         return x
