c37fda363dbb7ff7a8422471ace1a0a019120dd1
Md. Rakib <muhammad.rakib2299@gmail.com>
2026-03-24 12:37:56 +0600

    leete code 64

---
+++ leetecode/64.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        
+        dp = [0] * n
+        
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[j] = grid[i][j]
+                elif i == 0:
+                    dp[j] = dp[j-1] + grid[i][j]
+                elif j == 0:
+                    dp[j] = dp[j] + grid[i][j]
+                else:
+                    dp[j] = min(dp[j], dp[j-1]) + grid[i][j]
+        
+        return dp[-1]
