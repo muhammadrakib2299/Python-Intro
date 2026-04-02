8fcd2d26817e53b4c799fd8bdd25c6b456139822
Md. Rakib <muhammad.rakib2299@gmail.com>
2026-04-02 12:32:08 +0600

    leete code 79

---
+++ leetecode/79.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        
+        def dfs(i, j, k):
+            # k = index in word
+            if k == len(word):
+                return True
+            
+            # boundary + mismatch check
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]:
+                return False
+            
+            temp = board[i][j]
+            board[i][j] = '#'  # mark visited
+            
+            # explore 4 directions
+            found = (
+                dfs(i+1, j, k+1) or
+                dfs(i-1, j, k+1) or
+                dfs(i, j+1, k+1) or
+                dfs(i, j-1, k+1)
+            )
+            
+            board[i][j] = temp  # backtrack
+            return found
+        
+        for i in range(m):
+            for j in range(n):
+                if dfs(i, j, 0):
+                    return True
+        
+        return False
