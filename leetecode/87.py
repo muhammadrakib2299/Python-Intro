23f831d472f06e9110251e8274d294f32d89535f
Md. Rakib <muhammad.rakib2299@gmail.com>
2026-04-09 12:49:23 +0600

    leete code 87

---
+++ leetecode/87.py
@@ -0,0 +1,30 @@
+from functools import lru_cache
+
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        
+        @lru_cache(None)
+        def dfs(a, b):
+            # base case
+            if a == b:
+                return True
+            
+            # pruning: must have same characters
+            if sorted(a) != sorted(b):
+                return False
+            
+            n = len(a)
+            
+            for i in range(1, n):
+                
+                # Case 1: no swap
+                if dfs(a[:i], b[:i]) and dfs(a[i:], b[i:]):
+                    return True
+                
+                # Case 2: swap
+                if dfs(a[:i], b[n-i:]) and dfs(a[i:], b[:n-i]):
+                    return True
+            
+            return False
+        
+        return dfs(s1, s2)
