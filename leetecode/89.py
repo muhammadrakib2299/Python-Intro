7dba95e6dbff893b2e461508efb173ddff043559
Md. Rakib <muhammad.rakib2299@gmail.com>
2026-04-10 12:50:09 +0600

    89

---
+++ leetecode/89.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        res = []
+        for i in range(1 << n):  # 2^n
+            res.append(i ^ (i >> 1))
+        return res
