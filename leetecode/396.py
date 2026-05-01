8eae19079821e8b4c4e524eb12ac076c303f28bf
Md. Rakib <muhammad.rakib2299@gmail.com>
2026-05-01 21:13:18 +0600

    396

---
+++ leetecode/396.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        n = len(nums)
+        total_sum = sum(nums)
+        
+        # Compute F(0)
+        F = sum(i * num for i, num in enumerate(nums))
+        max_val = F
+        
+        # Compute F(1) to F(n-1)
+        for k in range(1, n):
+            F = F + total_sum - n * nums[n - k]
+            max_val = max(max_val, F)
+        
+        return max_val
